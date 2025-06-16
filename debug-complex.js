7a7c07260502413a0fde618ab77a2d2237675a96
Jim Stott <jstott@montra.io>
2025-06-16 14:56:35 -0400

    Remove test-transformation.js and add .npmignore to exclude debug, test files, and configuration settings

--- debug-complex.js
+++
@@ -1,8 +0,0 @@
-const Parser = require("./lib/parser").Parser;
-
-console.log("=== Testing complex OR expression ===");
-const parser = new Parser();
-const ast = parser.query("$filter=(uom is nullOrEmpty or category ne 'Assigned')");
-
-console.log("Full AST structure:");
-console.log(JSON.stringify(ast, null, 2));
