c78adab8ee3e77633f0901e266862a77e548346d
Jim Stott <jstott@montra.io>
2025-06-16 16:53:20 -0400

    Add tests for advanced null checks in parser
    - Added a test case to verify that the parser correctly handles the "is not null" condition.
    - Added a test case for the "is null" condition to ensure proper parsing.
    - Introduced a test case for "is nullOrEmpty" to check combined null and empty string conditions.

--- test/parser-tableName.spec.js
+++ test/parser-tableName.spec.js
@@ -7,9 +7,25 @@ describe("Parser", () => {
 
     it("advanced is not null", () => {
         var parser = new Parser();
+        var ast = parser.query("$filter=vAsset__state is not null");
+        console.log(ast);
+        expect(ast.value.options[0].value.value.value).to.equal("\"vAsset__state\" IS NOT NULL");
+    });
+
+    it("advanced is null", () => {
+        var parser = new Parser();
         var ast = parser.query("$filter=vAsset__state is null");
         console.log(ast);
         expect(ast.value.options[0].value.value.value).to.equal("\"vAsset.state\" IS NULL");
+    });
+
+    it("advanced is nullOrEmpty", () => {
+        var parser = new Parser();
+        var ast = parser.query("$filter=vAsset__state is nullOrEmpty");
+        console.log(ast);
+
+        expect(ast.value.options[0].value.value.value).to.equal("( \"vAsset__state\" IS NULL OR \"vAsset__state\" = '' )");
+
     });
 
 });
